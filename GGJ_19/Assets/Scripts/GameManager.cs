795da5055b4f5f6ae3fa264dd0bc8723247d26e0
Preside <thepreside93@gmail.com>
2019-01-25 23:41:46 +0100

    Scene Management Setup

---
+++ GGJ_19/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public string[] scenesName;
+
+    private int currentScene = 0;
+
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(scenesName[0],LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) { ChangeInsideScene(); }
+    }
+
+    public void ChangeInsideScene()
+    {
+        // TODO maybe use coroutines
+        SceneManager.UnloadSceneAsync(scenesName[currentScene]);
+        if (++currentScene >= scenesName.Length) { currentScene = 0; }
+        SceneManager.LoadSceneAsync(scenesName[currentScene],LoadSceneMode.Additive);
+    }
+}
