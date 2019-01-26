981d412edf133d6a7b1ec0dca6b04635859874c8
RobertsteinOfficial <robertovenezia95@gmail.com>
2019-01-26 15:57:36 +0100

    BuxFix

---
+++ GGJ_19/Assets/Scripts/RoulotteController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoulotteController : MonoBehaviour
+{
+    public Transform car;
+
+    private MinigameManager MGM;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        transform.LookAt(car);
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(car.position.x, transform.position.y, transform.position.z), 0.1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Obstacle")
+        {
+
+            if (MGM == null)
+            {
+                MGM = FindObjectOfType<MinigameManager>();
+            }
+
+            MGM.DoBump(MinigameManager.BumpType.Stone);
+        }
+        else if (other.tag == "Hole")
+        {
+            if (MGM == null)
+            {
+                MGM = FindObjectOfType<MinigameManager>();
+            }
+
+            MGM.DoBump(MinigameManager.BumpType.Hole);
+        }
+
+        if (other.tag == "Offroad")
+        {
+            if (MGM == null)
+            {
+                MGM = FindObjectOfType<MinigameManager>();
+            }
+
+            MGM.EnableOffroad(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Offroad")
+        {
+            if (MGM == null)
+            {
+                MGM = FindObjectOfType<MinigameManager>();
+            }
+
+            MGM.EnableOffroad(false);
+        }
+    }
+}
